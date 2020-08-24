036d18358b7528e69c3d2360b1ae8be68b37fddd
Chev <chev@chevtek.io>
2020-08-23 23:26:02 -0600

    Update temp holdem message.

--- commands/holdem.ts
+++ commands/holdem.ts
@@ -3,5 +3,5 @@ export const command = "holdem";
 export const description = "Begin a game of Texas Hold'em!";
 
 export function handler ({ message }) {
-  message.reply("success");
+  message.reply("still in development");
 }
