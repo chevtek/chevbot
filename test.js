dba414c9ee916b1c3bec7162db11e1959eef267d
Chev <chev@chevtek.io>
2020-09-03 17:41:02 -0600

    Removing unused files.

--- test.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const Client = require("discord.js").Client;
-const { DISCORD_BOT_TOKEN } = process.env;
-const client = new Client();
-client.login(DISCORD_BOT_TOKEN).then(() => {
-  client.users.fetch("251192242834767873").then(user => {
-    console.log(user);
-  });
-});
--- test.ts
+++
@@ -1,9 +0,0 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-// import discordClient from "./discord-client";
-
-// const user = discordClient.users.cache.get("251192242834767873");
-
-// user?.dmChannel.messages.cache.forEach(message => {
-//   message.delete();
-// });
