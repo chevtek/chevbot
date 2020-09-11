dd97d2c65c7d61819360899383c042df3b1c970c
Chev <chev@chevtek.io>
2020-09-11 12:06:16 -0600

    Command to update listen channels.

---
+++ commands/listento.ts
@@ -0,0 +1,18 @@
+import { Message } from "discord.js";
+
+export const command = ["listento <title..>", "lt"];
+
+export const description = "Modifies the listen text and voice channel names.";
+
+export async function handler ({ discord, title }) {
+  const message = discord.message as Message;
+  title = title.join(" ");
+
+  const textChannel = message.guild!.channels.cache.get("752945543092895865");
+  const voiceChannel = message.guild!.channels.cache.get("709780789218508821");
+
+  await textChannel!.setName(title);
+  await voiceChannel!.setName(`${title} 🎧`);
+  
+  await message.reply(`Listen channels have been updated to "${title}" successfully.`);
+} 
