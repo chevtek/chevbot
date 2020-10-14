80887a8db0a2fb091fa2847c3e586fe44452c6bd
Chev <chev@chevtek.io>
2020-10-14 10:49:40 -0600

    Change slogan names once a day.

--- utilities/on-this-day.ts
+++ utilities/on-this-day.ts
@@ -5,25 +5,24 @@ import { MessageEmbed } from "discord.js";
 
 export default async function () {
   const now = moment().tz("America/Denver");
-  if (now.hour() === 6 && now.minute() === 0) {
-    const {data:{data:{
-      Events: events,
-      Births: births,
-      Deaths: deaths
-    }}} = await axios.get("https://history.muffinlabs.com/date");
-    const event = events[Math.floor(Math.random() * events.length)];
-    const birth = births[Math.floor(Math.random() * births.length)];
-    const death = deaths[Math.floor(Math.random() * deaths.length)];
-    event.markdown = event.html.replace(/<a href="([^"]+)"[^>]+>([^<]+)<\/a>/g, (match, href, text) => `[${text}](${href})`);
-    birth.markdown = birth.html.replace(/<a href="([^"]+)"[^>]+>([^<]+)<\/a>/g, (match, href, text) => `[${text}](${href})`);
-    death.markdown = death.html.replace(/<a href="([^"]+)"[^>]+>([^<]+)<\/a>/g, (match, href, text) => `[${text}](${href})`);
-    const embed = new MessageEmbed()
-      .setTitle(`On this day in history!`)
-      .setThumbnail(discordClient.user!.avatarURL({ format: "png" })!)
-      .setDescription(`**Event**\n${event.markdown}\n\n**Birth**\n${birth.markdown}\n\n**Death**\n${death.markdown}`)
-      .setColor("#00ff00")
-      .setFooter("Powered by MuffinLabs.com", "https://github.com/muffinista.png");
-    const channels = discordClient.guilds.cache.map(guild => guild.systemChannel);
-    await Promise.all(channels.map(channel => channel?.send(embed)));
-  }
+  if (now.hour() !== 6 || now.minute() !== 0) return;
+  const {data:{data:{
+    Events: events,
+    Births: births,
+    Deaths: deaths
+  }}} = await axios.get("https://history.muffinlabs.com/date");
+  const event = events[Math.floor(Math.random() * events.length)];
+  const birth = births[Math.floor(Math.random() * births.length)];
+  const death = deaths[Math.floor(Math.random() * deaths.length)];
+  event.markdown = event.html.replace(/<a href="([^"]+)"[^>]+>([^<]+)<\/a>/g, (match, href, text) => `[${text}](${href})`);
+  birth.markdown = birth.html.replace(/<a href="([^"]+)"[^>]+>([^<]+)<\/a>/g, (match, href, text) => `[${text}](${href})`);
+  death.markdown = death.html.replace(/<a href="([^"]+)"[^>]+>([^<]+)<\/a>/g, (match, href, text) => `[${text}](${href})`);
+  const embed = new MessageEmbed()
+    .setTitle(`On this day in history!`)
+    .setThumbnail(discordClient.user!.avatarURL({ format: "png" })!)
+    .setDescription(`**Event**\n${event.markdown}\n\n**Birth**\n${birth.markdown}\n\n**Death**\n${death.markdown}`)
+    .setColor("#00ff00")
+    .setFooter("Powered by MuffinLabs.com", "https://github.com/muffinista.png");
+  const channels = discordClient.guilds.cache.map(guild => guild.systemChannel);
+  await Promise.all(channels.map(channel => channel?.send(embed)));
 }
--- utilities/slogan-checker.ts
+++ utilities/slogan-checker.ts
@@ -5,43 +5,37 @@ import moment from "moment-timezone";
 export default async function () {
   const { sloganMembers, sloganTemplates } = db;
   const now = moment().tz("America/Denver");
-  if (now.minute() !== 0) return;
-  console.log("Running slogan checker...");
-  const roll = Math.floor(Math.random() * 12);
-  console.log(`Rolled a ${roll}.`);
-  if (roll === 0) {
-    console.log("Triggered!");
-    const { resources: members } = await sloganMembers!.items.readAll({ partitionKey: "/_partitionKey" }).fetchAll();
-    const { resources: templates } = await sloganTemplates!.items.readAll({ partitionKey: "/_partitionKey"}).fetchAll();
-    console.log(`${templates.length} templates found.`);
-    let unusedTemplates = templates.filter(template => !template.used);
-    console.log(`${unusedTemplates.length} templates have not been used yet.`);
-    if (unusedTemplates.length < members.length) {
-      console.log(`There are ${members.length} sloganme members but only ${unusedTemplates.length} unused templates. Resetting template usage...`);
-      await sloganTemplates!.items.bulk(templates.map(template => {
-        template.used = false;
-        return {
-          operationType: "Upsert",
-          resourceBody: template
-        };
-      }));
-      console.log("All templates have been marked unused.");
-      unusedTemplates = templates.filter(template => !template.used);
-    }
-    await Promise.all(members.map(async memberDoc => {
-      const guild = discordClient.guilds.cache.get(memberDoc.guildId);
-      const member = guild!.members.cache.get(memberDoc.id!);
-      let username = member!.user.username;
-      const randomTemplate = unusedTemplates[Math.floor(Math.random() * unusedTemplates.length)];
-      let renderedTemplate = randomTemplate.template.replace(/{{name}}/g, username);
-      while (renderedTemplate.length > 32) {
-        username = username.slice(0, username.length - 1);
-        renderedTemplate = randomTemplate.template.replace(/{{name}}/g, username);
-      }
-      await member?.setNickname(renderedTemplate);
-      randomTemplate.used = true;
-      await sloganTemplates!.items.upsert(randomTemplate);
+  if (now.hour() !== 6 || now.minute() !== 0) return;
+  const { resources: members } = await sloganMembers!.items.readAll({ partitionKey: "/_partitionKey" }).fetchAll();
+  const { resources: templates } = await sloganTemplates!.items.readAll({ partitionKey: "/_partitionKey"}).fetchAll();
+  console.log(`${templates.length} templates found.`);
+  let unusedTemplates = templates.filter(template => !template.used);
+  console.log(`${unusedTemplates.length} templates have not been used yet.`);
+  if (unusedTemplates.length < members.length) {
+    console.log(`There are ${members.length} sloganme members but only ${unusedTemplates.length} unused templates. Resetting template usage...`);
+    await sloganTemplates!.items.bulk(templates.map(template => {
+      template.used = false;
+      return {
+        operationType: "Upsert",
+        resourceBody: template
+      };
     }));
-    console.log("Member names have been changed :)");
+    console.log("All templates have been marked unused.");
+    unusedTemplates = templates.filter(template => !template.used);
   }
+  await Promise.all(members.map(async memberDoc => {
+    const guild = discordClient.guilds.cache.get(memberDoc.guildId);
+    const member = guild!.members.cache.get(memberDoc.id!);
+    let username = member!.user.username;
+    const randomTemplate = unusedTemplates[Math.floor(Math.random() * unusedTemplates.length)];
+    let renderedTemplate = randomTemplate.template.replace(/{{name}}/g, username);
+    while (renderedTemplate.length > 32) {
+      username = username.slice(0, username.length - 1);
+      renderedTemplate = randomTemplate.template.replace(/{{name}}/g, username);
+    }
+    await member?.setNickname(renderedTemplate);
+    randomTemplate.used = true;
+    await sloganTemplates!.items.upsert(randomTemplate);
+  }));
+  console.log("Member names have been changed :)");
 }
